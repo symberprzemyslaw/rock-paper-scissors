a63682a4237e2324402fcbe2777454b141807434
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-08-08 01:58:19 +0200

    Starting to make some UI

--- index.js
+++ index.js
@@ -1,67 +1,52 @@
-
 const game = {
-    DOM : {
+    DOM: {
         scores: document.querySelector("#scores"),
-        message: document.querySelector('#message'),
-        rockBtn: document.querySelector('#rock'),
-        paperBtn: document.querySelector('#paper'),
-        scissorsBtn: document.querySelector('#scissors'),
-        btns : [rockBtn, paperBtn, scissorsBtn]
+        message: document.querySelector("#message"),
+        rockBtn: document.querySelector("#rock"),
+        paperBtn: document.querySelector("#paper"),
+        scissorsBtn: document.querySelector("#scissors")
     },
-    playerSelection,
-    computerScore : 0,
-    playerScore : 0,
+    playerScore: 0,
+    computerScore: 0,
+    choices: ["rock", "paper", "scissors"],
 
     getComputerChoice() {
-        choices = ["rock", "paper", "scissors"]
-        let randomNum = Math.floor(Math.random() * 3)
-        return choices[randomNum]
+        const randomNum = Math.floor(Math.random() * this.choices.length);
+        return this.choices[randomNum];
     },
     playRound(computerSelection, playerSelection) {
-
-    if (computerSelection === "rock" && playerSelection === "paper") {
-        playerScore++
-        return "You win! Paper beats rock!"
-    } else if (computerSelection === "paper" && playerSelection === "scissors") {
-        playerScore++
-        return "You win! Scissors beats paper!"
-    } else if (computerSelection === "scissors" && playerSelection === "rock") {
-        playerScore++
-        return "You win! rock beats scissors"
-    } else if (computerSelection === playerSelection) {
-        return "Its a tie!"
-    } else {
-        computerScore++
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+        if (computerSelection === playerSelection) {
+            return "It's a tie!";
+        } else if (
+            (computerSelection === "rock" && playerSelection === "paper") ||
+            (computerSelection === "paper" && playerSelection === "scissors") ||
+            (computerSelection === "scissors" && playerSelection === "rock")
+        ) {
+            this.playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        } else {
+            this.computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}!`;
+        }
+    },
+    logic(playerSelection) {
+        const { message, scores } = this.DOM;
+        const computerSelection = this.getComputerChoice();
+        message.textContent = this.playRound(computerSelection, playerSelection);
+        scores.textContent = `Player score: ${this.playerScore} Computer score: ${this.computerScore}`;
+        if (this.playerScore >= 5) {
+            message.textContent = "Player Wins!";
+        } else if (this.computerScore >= 5) {
+            message.textContent = "Computer Wins!";
+        }
+    },
+    init() {
+        const { rockBtn, paperBtn, scissorsBtn } = this.DOM;
+        this.DOM.btns = [rockBtn, paperBtn, scissorsBtn];
+        this.DOM.btns.forEach(btn => {
+            btn.addEventListener("click", () => this.logic(btn.id));
+        });
     }
-}
-}
+};
 
-
-// Handling user inputs, but its very, verylong and nasty
-//it will be better if it was a function inside a game with two parameters and also
-//extraction of this funtions here to simply btn => game.chose(paper)
-
-
-const logic = playerSelection => {
-    const [message, scores] = game.DOM
-    const [playerScore, computerScore] = game
-    let computerSelection = getComputerChoice()
-    message.textContent = playRound(computerSelection, playerSelection)
-    scores.textContent = `Player score: ${playerScore} Computer score: ${computerScore}`
-    if (playerScore >= 5) {
-        message.textContent = "Player Wins!"
-    } else if (computerScore >= 5) {
-        message.textContent = "Computer Wins!"
-    }
-}
-
-rockBtn.addEventListener('click', function () {
-    logic('rock')
-})
-paperBtn.addEventListener('click', function () {
-    logic('paper')
-})
-scissorsBtn.addEventListener('click', function () {
-    logic('scissors')
-})
+game.init();
